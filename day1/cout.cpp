248869f081e60a14186a648e899dcd07b5eea30c
duwei <duwei19961021@163.com>
2020-12-11 00:13:32 +0800

    add cout

---
+++ day1/cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello ";
+	cout << "world";
+	cout << endl;
+	return 0;
+}
+/*
+ * cout: 对象
+ * <<: 插入运算符
+ * "hello ": 字符串
+ * endl: 表示重起一行，在输出流中插入endl将导致屏幕光标移到下一行开头，endl确保程序继续运行之前刷新输出(将其立即显示在屏幕上)
+ * */
--- day1/myfirst.cpp
+++ day1/myfirst.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+/*
 int main()
 {
 	using namespace std;
 	cout << "hello world" << endl;
+	return 0;
+}
+*/
+
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
 	return 0;
 }
