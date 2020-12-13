2f3ed30d8489d2d8b42f12a9ae8895a78cdb28bd
duwei <duwei19961021@163.com>
2020-12-13 18:31:27 +0800

    初始化列表语法

--- day3/classInitList.cpp
+++ day3/classInitList.cpp
@@ -4,10 +4,15 @@ using namespace std;
 class Person
 {
 	public:
-		Person() :m_A(10),m_B(20),m_C(30) // 初始化列表初始化属性
-			   {
+		//Person() :m_A(10),m_B(20),m_C(30) // 初始化列表初始化属性
+		//{
 			   
-			   }
+	    //}
+
+		Person(int a, int b, int c):m_A(a),m_B(b),m_C(c)
+		{
+		
+		}
 	int m_A;
 	int m_B;
 	int m_C;
@@ -15,7 +20,7 @@ class Person
 
 int main()
 {
-	Person p;
+	Person p(1,2,3);
 	cout << "p.m_A = " << p.m_A << endl;
 	cout << "p.m_B = " << p.m_B << endl;
 	cout << "p.m_C = " << p.m_C << endl;
