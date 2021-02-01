662799c8b923a8ad49b2c714315e14d88d7d57d6
杜维 <duwei19961021@163.com>
2021-02-01 22:25:06 +0800

    引用

--- day2/appoint.cpp
+++ day2/appoint.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int& Add(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
 int main()
 {
+	/*
 	int a = 10;
 	int &b = a;
 
@@ -13,7 +20,7 @@ int main()
 	cout << "a = " << a << endl;
     cout << "b = " << b << endl;
 	
-	cout << "&a = " << &a << endl;
+	cout << "&a = " << &a << endl; // a b 地址相同
 	cout << "&b = " << &b << endl;
 
 	int n = 10;
@@ -24,6 +31,24 @@ int main()
 	cout << "n = " << n  << endl;
 	cout << "m = " << m << endl;
 	cout << "c = " << c << endl;
+
+	// 指针和引用在初始化赋值时,权限可以缩小但是不可以放大
+	const int n1 = 10;
+	// int& m1 = n1;
+	const int& m1 = n1;
+	int p = 20;
+	const int &q = p;
+
+	double dd = 2.2222;
+	int i = dd; // dd在这里经过复杂的运算转换成一个整数,产生一个临时变量,i等于这个临时变量,而临时变量具有常性。
+	cout << "i = " << i << endl;
+	// int& ri = dd; error: invalid initialization of reference of type ‘int&’ from expression of type ‘double’
+	const int& ri = dd;	
+	*/
+
+	int& ret = Add(1,2); 
+	Add(3,4);
+	cout << "Add(1,2) = " << ret << endl;
 
 	return 0;
 }
