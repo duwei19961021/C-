117f1f9af1e3ac46236169cb026c1c4e019be666
杜维 <duwei19961021@163.com>
2021-02-06 23:30:41 +0800

    日期类 -= += - + 的实现

--- day4/Class_Date/Date.cpp
+++ day4/Class_Date/Date.cpp
@@ -8,7 +8,7 @@ void Date::PrintDate()
 // 根据年月获取当月的天数
 int Date::GetMonthDay(int year, int month)
 {
-	int monthArray[13] = {0, 31, 28, 31, 30, 30, 31, 30, 31, 30, 31, 30, 31};
+	int monthArray[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	if(((year%4 == 0 && year%100 != 0) || year%400 == 0) && month == 2)
 	{
 		monthArray[2] = 29;
@@ -48,7 +48,7 @@ Date::Date(const Date& d)
 	this->_day = d._day;
 }
 
-Date& Date:: operator=(const Date& d)
+Date& Date::operator=(const Date& d)
 {
 	this->_year = d._year;
 	this->_month = d._month;
@@ -60,4 +60,49 @@ Date& Date:: operator=(const Date& d)
 Date::~Date()
 {
 
+}
+
+Date& Date::operator+=(int day)
+{
+	this->_day += day; // 将日期先加上,然后根据天数计算月份该加多少,年该加多少
+	while(this->_day > GetMonthDay(this->_year, this->_month)) // 当相加之后的天数大于本月天数时
+	{
+		this->_day -= GetMonthDay(this->_year, this->_month); // day要减去本月天数
+		this->_month++; // month进一
+		if(this->_month > 12) // month大于13 year要进一且month要减去一年即12个月
+		{
+			this->_year++;
+			this->_month -= 12;
+		}
+	}
+	return *this;
+}
+
+Date Date::operator+(int day)
+{
+	Date ret(*this); // 利用拷贝构造函数创建一个新的ret,然后对ret进行加等,不会影响原值。
+	ret += day;
+	return ret;
+}
+
+Date& Date::operator-=(int day)
+{
+	this->_day -= day;		
+	while(this->_day < 1)
+	{
+		this->_day += GetMonthDay(this->_year, --this->_month); // 向上个月借天数
+		if(this->_month == 1) // 这里特别注意：当month为1的时候要向12月借天数,虽然进入循环month会等于13,但是再次进入循环时,month会先自减即12月
+		{
+			this->_month += 12;
+			this->_year--;
+		}
+	}
+	return *this;
+}
+
+Date Date::operator-(int day)
+{
+	Date ret(*this);
+	ret -= day;
+	return ret;
 }
--- day4/Class_Date/test.cpp
+++ day4/Class_Date/test.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-	Date d(2024,2,29);
-	Date d2 = d;
+	Date d(2020,3,24);
+	Date d2(d);
+	d2.PrintDate();
+	d2 -= 90;
+	d2.PrintDate();
 	return 0;
 }
